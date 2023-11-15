30432c90080a238569bf3aa48b76c09716047b0c
lukeleech <lukeyoobee@gmail.com>
2023-11-15 13:52:34 +1300

    temp changes

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ import { Footer } from "./components/footer";
 import { Shop } from "./pages/shop/shop";
 import { CreateListing } from "./pages/shop/createlisting";
 import { Cart } from "./pages/cart/cart";
-import { ShopContextProvider } from "./context/shop-context";
 import { Listing } from "./pages/shop/listing";
 import { Featured } from "./pages/shop/featured";
 //import { Login } from "./pages/account/login";
@@ -15,23 +14,21 @@ import { AccountPage } from "./pages/account/accountpage";
 function App() {
   return (
     <div className="App">
-      <ShopContextProvider>
-        <Router>
-          <Navbar />
-          <Routes>
-            <Route path="/" element={<Shop />} />
-            <Route path="/cars" element={<Featured />} />
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Shop />} />
+          <Route path="/cars" element={<Featured />} />
 
-            <Route path="/cart" element={<Cart />} />
-            <Route path="/listing/:id" element={<Listing />} />
-            <Route path="/login" />
-            <Route path="/register" />
-            <Route path="/createlisting" element={<CreateListing />} />
-            <Route path="/account" element={<AccountPage />} />
-          </Routes>
-          <Footer />
-        </Router>
-      </ShopContextProvider>
+          <Route path="/cart" element={<Cart />} />
+          <Route path="/listing/:id" element={<Listing />} />
+          <Route path="/login" />
+          <Route path="/register" />
+          <Route path="/createlisting" element={<CreateListing />} />
+          <Route path="/account" element={<AccountPage />} />
+        </Routes>
+        <Footer />
+      </Router>
     </div>
   );
 }
